93e48e51db495c354ab835b7af93737123b2c008
ipolyako <ipolyako@hotmail.com>
2025-05-31 10:52:17 -0500

    fix: improve Privacy page layout on mobile devices

--- src/pages/Privacy.tsx
+++ src/pages/Privacy.tsx
@@ -1,12 +1,11 @@
-
 import { Navigation } from "@/components/Navigation";
 
 const Privacy = () => {
   return (
-    <div className="min-h-screen bg-background">
+    <div className="page-container bg-background">
       <Navigation />
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-24">
-        <div className="w-full aspect-[16/9]">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-8">
+        <div className="w-full h-[calc(100vh-8rem)]">
           <iframe
             src="https://drive.google.com/file/d/1AOrV0qM5A031NAJZ6tEm8kBwpgto0gwk/preview"
             className="w-full h-full border-0"
